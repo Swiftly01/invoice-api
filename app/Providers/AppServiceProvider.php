22b16feb53ab91e5048d891b620c419caa7bbbb9
Swiftly01 <kareemkazeem100@gmail.com>
2025-09-23 19:57:26 -0700

    chore(invoices): bind InvoiceRepository to InvoiceRepositoryInterface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\InvoiceRepositoryInterface;
+use App\Repositories\InvoiceRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(InvoiceRepositoryInterface::class, InvoiceRepository::class);
     }
 
     /**
