4cc31b286591bd14ce15d8aebf0153767cfc24c8
Swiftly01 <kareemkazeem100@gmail.com>
2025-09-23 19:24:53 -0700

    feat(model): add fillable and casts to Invoice model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -9,4 +9,20 @@ class Invoice extends Model
 {
     /** @use HasFactory<\Database\Factories\InvoiceFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'invoice_number',
+        'customer_name',
+        'customer_email',
+        'amount',
+        'status',
+        'due_date',
+
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'due_date' => 'date:Y-m-d',
+
+    ];
 }
